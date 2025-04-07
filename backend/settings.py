305fba8bc5c9c9aa9bc647c16e9c35d5097dea6d
Tantoy, James Ryan <100672289+Honaska@users.noreply.github.com>
2025-04-07 22:14:47 +0800

    Allowed_host changed

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split("")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+if ALLOWED_HOSTS is not None:
+    ALLOWED_HOSTS = ALLOWED_HOSTS.split(",")
+else:
+    ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
