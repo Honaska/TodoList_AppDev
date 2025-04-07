8e9d5b350c28d42570156712cc5f05d0534d9662
Tantoy, James Ryan <100672289+Honaska@users.noreply.github.com>
2025-04-07 22:35:33 +0800

    yodate

--- backend/settings.py
+++ backend/settings.py
@@ -27,12 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
-if ALLOWED_HOSTS is not None:
-    ALLOWED_HOSTS = ALLOWED_HOSTS.split(",")
-else:
-    ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
