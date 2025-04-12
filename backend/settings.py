d04ef4fc8a1529e26040e9c0ae34969e440115b7
Tantoy, James Ryan <100672289+Honaska@users.noreply.github.com>
2025-04-12 21:25:19 +0800

    Improved

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'Secret_key_here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://todolist-appdev.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['todolist-appdev.onrender.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
